4c79efc93e0247c7a41fec7a52ae729afcbfd932
PolinaDrukova <70808551+PolinaDrukova@users.noreply.github.com>
2020-10-08 20:45:37 +0300

    обновлен BattleField

--- src/com/company/BattleField.java
+++ src/com/company/BattleField.java
@@ -1,100 +1,103 @@
 package com.company;
 
-import java.util.ArrayList;
+import com.company.objects.Point;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 
 public class BattleField {
-    public GameElements[][] elements;
-    public ArrayList<Ship> ships;
+    public static final Logger log = Logger.getLogger(BattleField.class.getName());
 
-    public BattleField(GameElements[][] elements, ArrayList<Ship> ships) {
-        this.elements = elements;
-        this.ships = ships;
+    public final int width = 10;
+    public final int height = 10;
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                this.elements[j][i] = new GameElements(j, i);
-            }
-        }
-        this.putShips();
+    private char[] head;
+    private char[][] cells;
+    private IMapObject[][] objects;
+    Map<Character, Integer> points = new HashMap<>();
+
+    public BattleField() {
+        head = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        cells = new char[width][height];
+        objects = new IMapObject[width][height];
+
+        points.put('A', 0);
+        points.put('B', 1);
+        points.put('C', 2);
+        points.put('D', 3);
+        points.put('E', 4);
+        points.put('F', 5);
+        points.put('G', 6);
+        points.put('H', 7);
+        points.put('I', 8);
+        points.put('J', 9);
     }
 
-    public void putShips() {
-        GameElements element;
-        for (int i = 0; i < 10; ++i) { //заполнение поля водой
-            for (int j = 0; j < 10; ++j) {
-                element = this.elements[j][i];
-                element.state = State.water;
-                element.shoot = false;
+    public void addObject(IMapObject object) {
+        Point position = object.getPosition();
+        if (isValidCoord(position)) {
+            objects[position.x][position.y] = object;
+        } else {
+            log.warning("can't  add map object");
+        }
+    }
+
+
+    public boolean isCollide(Point position) { // дописать границы
+        return objects[position.x][position.y] != null;
+    }
+
+
+    public boolean isValidCoord(Point point) {
+        return point.x >= 0 && point.x < width && point.y >= 0 && point.y < height;
+    }
+
+    public void clean() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                cells[x][y] = ' ';
             }
         }
+    }
 
-        this.ships = new ArrayList<>();
+    private void update() {
+        clean();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
 
-        for (int i = 1; i > 0; i++) { //добавление на поле кораблей
-            for (int j = 5 - i; j > 0; j--) {
-                Ship ship = new Ship(this, i);
-                this.ships.add(ship);
-            }
-        }
+                IMapObject object = objects[x][y];
+                if (object != null) {
+                    Point position = object.getPosition();
 
-        for (int i = 0; i < 10; i++) { //добавление запретной зон
-            for (int j = 0; j < 10; j++) {
-                element = this.elements[i][j];
-                if (element.state == State.border) {
-                    element.state = State.water;
+                    cells[position.x][position.y] = object.getView();
                 }
             }
-
         }
-
     }
 
-    public boolean shot(int x, int y) {
-        boolean shot = false;
-        State state = this.getShipState(x, y);
-        elements[x][y].shoot = true;
-
-        if (state == State.enWell) {
-            shot = true;
-            Ship ship = elements[x][y].ship;
-            if (ship.health != 0) { //если здоровье не нулевое, вычитается один пункт и присваивается тип ранен
-                ship.health--;
-                if (ship.health == 0) { //если здоровье нулевое присваивается тип убит
-                    ship.state = ShipState.killed;
-                } else {
-                    ship.state = ShipState.injured;
-                    elements[x][y].state = State.injured;
+    public void draw() {
+        update();
+        int coordX = 0;
+        int coordY = 0;
+        System.out.print(' ');
+        for (int x = 0; x < width; x++) {
+            System.out.print(' ');
+            System.out.print(head[coordX]);
+            coordX += 1;
+        }
+        System.out.println();
+        for (int y = 0; y < height; y++) {
+            System.out.print(coordY++);
+            for (int x = 0; x < width; x++) {
+                if (!(x == 0 && coordY == height + 1)) {
+                    System.out.print(' ');
                 }
+                System.out.print(cells[x][y]);
+
             }
-        } else {
-            if ((state == State.border) ||    //если попало на границу или в воду, тип "мимо"
-                    (state == State.water)) {
-                this.setShip(x, y, State.missed);
-            }
-        }
-        return shot;
-    }
-
-
-    public boolean setShip(int x, int y, State state) {
-        if (this.check_Bound(x, y)) {
-            this.elements[x][y].state = state;
-        }
-
-        return true;
-    }
-
-    public boolean check_Bound(int x, int y) {
-        return x >= 0 && x <= 9 && y >= 0 && y <= 9;
-    }
-
-
-    public State getShipState(int x, int y) {//получаем тип элемента
-        if (check_Bound(x, y)) {
-            return elements[x][y].state;
-        } else {
-            return State.empty;
+            System.out.println('|');
         }
     }
 }
-
