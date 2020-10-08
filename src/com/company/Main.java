f08c79d19bbde9b07692f11daf8b6081d54cfa38
PolinaDrukova <70808551+PolinaDrukova@users.noreply.github.com>
2020-10-08 20:45:57 +0300

    обновлен Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,29 @@
 package com.company;
 
-import javax.swing.*;
+import com.company.enum_state.DeckCount;
+import com.company.enum_state.GameState;
+import com.company.enum_state.Orientation;
+import com.company.Input;
+import com.company.objects.Point;
+import com.company.objects.Ship;
 
 public class Main {
 
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(800, 500);
-        frame.setVisible(true);
+        BattleField playerMap = new  BattleField();
+        BattleField enemyMap = new  BattleField();
+
+        Input in = new Input(null);
+        GameState gs =  GameState.Fill;
+        Ship ship_d3 = Ship.make(playerMap, Orientation.Vertical, DeckCount.Three, new Point(2, 7));
+        Ship ship_d2 = Ship.make(playerMap, Orientation.Horizontal, DeckCount.Four, new Point(2, 5));
+        do {
+            System.out.println("---- My Map ----");
+            playerMap.draw();
+            System.out.println("---- Enemy Map ----");
+            enemyMap.draw();
+            in.process(gs);
+
+        } while (!in.isDone());
     }
 }
-
